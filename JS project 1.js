26454bffad6b43b5e592f33d25132a6b0ad3c121
Mohammed <m0imaaanahmed@gmail.com>
2025-07-15 13:58:04 +0100

    Add files via upload

---
+++ JS project 1.js
@@ -0,0 +1,15 @@
+console.log("Hello! I'm your coding fun fact guide!")
+let botName="Coding_Bot"
+let botLocation="Somewhere Avenue"
+let favoriteLanguage="JavaScript"
+
+let intro="My name is " +botName+" and I live on " +botLocation
+console.log(intro)
+console.log("My favorite programming language is " + favoriteLanguage)
+let codingFact=favoriteLanguage + " was developed by Brendan Eich in 1995 for Netscape. He completed the first version in a whopping 10 days to meet the pressing needs of the browser market!";
+console.log(codingFact)
+codingFact= favoriteLanguage +" isn't the real name of the language, it's ECMAScript. ECMAScript is usually referred to when talking about the version of this language, but in reality, “JavaScript” is a trademark registered by Sun Microsystems.";
+codingFact= favoriteLanguage +" is used by 95% of all websites on the internet.";
+console.log(codingFact)
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName+" from " +botLocation +".")
+
